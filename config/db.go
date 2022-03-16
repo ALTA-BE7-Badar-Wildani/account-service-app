1c0f0b6cb4b0bf0e714ddbc69a193f5d660c6f18
Badar Wildani <wildanie12@gmail.com>
2022-03-16 17:10:54 +0700

    add: konfigurasi database menggunakan dotenv

--- config/db.go
+++ config/db.go
@@ -1,12 +1,21 @@
 package config
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func MysqlConnect() *gorm.DB {
-	dsn := "root:root@tcp(localhost:3306)/account_service_app?charset=utf8mb4&parseTime=True&loc=Local"
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Tidak ada file konfigurasi dotenv tersedia")
+	}
+
+	dsn := os.Getenv("DSN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) 
 	if err != nil {
 		panic("Tidak dapat terhubung dengan database")
