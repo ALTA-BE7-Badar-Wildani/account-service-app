2f8d2a05c934f413561b10e4e350ef4e8a718f3e
Usamah Abdurrahman <abdurrahman.usamah07@gmail.com>
2022-03-16 23:21:37 +0700

    add: Input 1 - Fitur Add User

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,13 +1,48 @@
 package controllers
 
-import "fmt"
+import (
+	"account-service-app/config"
+	"account-service-app/entity"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	db = config.MysqlConnect()
+}
 
 func AddUser() {
-	fmt.Println("Add User")
+	addUser := entity.User{}
+	fmt.Println("Masukkan Nama:")
+	fmt.Scanln(&addUser.Nama)
+	fmt.Println("Masukkan Jenis Kelamin:")
+	fmt.Scanln(&addUser.JenisKelamin)
+	fmt.Println("Masukkan Alamat:")
+	fmt.Scanln(&addUser.Alamat)
+	fmt.Println("Masukkan Nomor HP:")
+	fmt.Scanln(&addUser.NomorHP)
+	fmt.Println("Masukkan Saldo:")
+	fmt.Scanln(&addUser.Saldo)
+	fmt.Println("Masukkan Email:")
+	fmt.Scanln(&addUser.Email)
+	fmt.Println("Masukkan Tanggal Lahir")
+	fmt.Scanln(&addUser.TanggalLahir)
+
+	tx := db.Save(&addUser)
+	if tx.Error != nil {
+		panic(tx.Error)
+	}
+	if tx.RowsAffected == 0 {
+		fmt.Println("add User failed")
+	}
+	fmt.Println("Add User Successfully")
 }
 
 func ListUser() {
-	fmt.Println("List User")
+
 }
 
 func UpdateUser() {
