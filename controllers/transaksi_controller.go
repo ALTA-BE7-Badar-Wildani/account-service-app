6c904bdf750f40db40232089a77d127efbb5beaa
Badar Wildani <wildanie12@gmail.com>
2022-03-16 14:53:56 +0700

    add: menambahkan print function  untuk testing

--- controllers/transaksi_controller.go
+++ controllers/transaksi_controller.go
@@ -1,18 +1,20 @@
 package controllers
 
+import "fmt"
+
 
 func TopUp() {
-	
+	fmt.Println("TopUp")
 }
 
 func Transfer() {
-	
+	fmt.Println("Transfer")
 }
 
 func HistoryTopUp() {
-	
+	fmt.Println("HistoryTopUp")
 }
 
 func HistoryTransfer() {
-	
+	fmt.Println("History Transfer")
 }
--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,17 +1,19 @@
 package controllers
 
+import "fmt"
+
 func AddUser() {
-	
+	fmt.Println("Add User")
 }
 
 func ListUser() {
-	
+	fmt.Println("List User")
 }
 
 func UpdateUser() {
-	
+	fmt.Println("Update User")
 }
 
 func DeleteUser() {
-	
+	fmt.Println("Delete User")
 }
