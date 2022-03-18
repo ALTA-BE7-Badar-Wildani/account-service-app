8be2771557f45c9e8bc7d5ff5e17ac9f55de3c08
Badar Wildani <wildanie12@gmail.com>
2022-03-18 10:41:17 +0700

    fix: menambahkan clearscreen memperbaiki tampilan

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"account-service-app/controllers"
 	"account-service-app/entity"
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func init() {
@@ -26,38 +28,66 @@ func main() {
 	fmt.Println("7) History Top-up")
 	fmt.Println("8) History Transfer")
 	fmt.Println("----------------------------")
-	fmt.Println("0) Exit")
+	fmt.Println("0) Keluar")
 	fmt.Println("----------------------------")
 	fmt.Print("Masukkan pilihan menu anda: ")
 	fmt.Scanln(&menu)
 	fmt.Println("----------------------------")
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
 
 	menuController(menu)
 }
 
 func menuController(menu string) {
+	clearScreen()
 	switch menu {
 	case "1":
+		fmt.Println("Add User")
+		fmt.Println("-------------------------")
 		controllers.AddUser()
 	case "2":
+		fmt.Println("List User")
+		fmt.Println("-------------------------")
 		controllers.ListUser()
 	case "3":
+		fmt.Println("Update User")
+		fmt.Println("-------------------------")
 		controllers.UpdateUser()
 	case "4":
+		fmt.Println("Delete User")
+		fmt.Println("-------------------------")
 		controllers.DeleteUser()
 	case "5":
+		fmt.Println("Topup")
+		fmt.Println("-------------------------")
 		controllers.TopUp()
 	case "6":
+		fmt.Println("Transfer")
+		fmt.Println("-------------------------")
 		controllers.Transfer()
 	case "7":
+		fmt.Println("History Top Up")
+		fmt.Println("-------------------------")
 		controllers.HistoryTopUp()
 	case "8":
+		fmt.Println("History Transfer")
+		fmt.Println("-------------------------")
 		controllers.HistoryTransfer()
+	case "0":
+		fmt.Println("----------------------------")
+		fmt.Println("  Terima kasih telah bertransaksi")
+		fmt.Println("----------------------------")
+		os.Exit(3)
 	}
-	fmt.Println("----------------------------")
-	fmt.Println("  Terima kasih telah bertransaksi")
+
+	fmt.Println("--------------------------- ")
+	fmt.Println("Tekan enter untuk lanjut... ")
+	fmt.Scanln()
+	clearScreen()
+	main()
+}
+
+func clearScreen() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
